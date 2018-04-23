e60ccfc140cb21ab3214cf8359cefc1ead9a9fc9
Wagner Bigal <wagner.bigal@cargox.com.br>
2018-04-23 11:30:04 -0300

    Setup simplecov gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
+require 'simplecov'
+
+SimpleCov.start
+SimpleCov.minimum_coverage 97
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
