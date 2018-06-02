51464d801a842c247213e38c9e4b5fdf77444a76
Wagner Bigal <wagner.bigal@cargox.com.br>
2018-06-02 15:25:33 -0300

    Update devise player's views

--- app/controllers/players/registrations_controller.rb
+++ app/controllers/players/registrations_controller.rb
@@ -2,14 +2,23 @@
 
 module Players
   class RegistrationsController < Devise::RegistrationsController
-    before_action :load_player_avatares, only: %i[new create]
+    before_action :load_player_avatares, only: %i[new create edit update]
     before_action :configure_sign_up_params, only: [:create]
+    before_action :configure_player_update_params, only: [:update]
 
     def new
       super
     end
 
     def create
+      super
+    end
+
+    def edit
+      super
+    end
+
+    def update
       super
     end
 
@@ -21,6 +30,11 @@ module Players
 
     def configure_sign_up_params
       devise_parameter_sanitizer.permit(:sign_up,
+                                        keys: %i[nickname player_avatar_id])
+    end
+
+    def configure_player_update_params
+      devise_parameter_sanitizer.permit(:account_update,
                                         keys: %i[nickname player_avatar_id])
     end
   end
