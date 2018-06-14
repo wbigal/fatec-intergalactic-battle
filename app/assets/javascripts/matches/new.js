1a77393caa52e61a01b20368e901cc248f9dbef7
Wellison Souza <wellison28@gmail.com>
2018-06-13 21:31:22 -0300

    refactor stylesheet

--- app/assets/javascripts/matches/new.js
+++ app/assets/javascripts/matches/new.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-	$('.capsule').click(function(){
-		$(this).find('input[type=radio]').prop('checked', true);
-		console.log($(this));
-		$('#selectTeam').modal();
-	})
+  $('.capsule').click(function(){
+    $(this).find('input[type=radio]').prop('checked', true);
+    console.log($(this));
+    $('#selectTeam').modal();
+  })
 });
