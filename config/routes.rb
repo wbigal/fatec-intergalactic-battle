95e37a355e7e4c59fa8ca9b78c48b8197bc42d45
Wagner Bigal <wagner.bigal@cargox.com.br>
2018-06-16 23:05:19 -0300

    Redirect player when someone joins for match

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount ActionCable.server => '/cable'
 
   devise_for :players, controllers: { registrations: 'players/registrations' }
   devise_for :users, controllers: { sessions: 'users/sessions' }
