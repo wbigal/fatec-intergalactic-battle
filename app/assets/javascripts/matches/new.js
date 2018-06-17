41fb1397696a5c2fa6f13a88add1d338281ef2ab
Wellison Souza <wellison28@gmail.com>
2018-06-17 14:57:26 -0300

    refactory code for merge

--- app/assets/javascripts/matches/new.js
+++ app/assets/javascripts/matches/new.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
   $('.capsule').click(function(){
     $(this).find('input[type=radio]').prop('checked', true);
-    console.log($(this));
     $('#selectTeam').modal();
   })
 });
